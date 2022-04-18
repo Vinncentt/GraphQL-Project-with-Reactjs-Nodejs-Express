f838d3183b328fdd9489daee6c0c75f3f06e86b1
sif1414 <ed14sif04@gmail.com>
2022-04-18 14:26:44 +0000

    work is done

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const PORT = 6969;
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schemas');
 const cors = require('cors');
+const mongoose = require('mongoose');
 app.use(cors());
 
 
@@ -11,6 +12,11 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+mongoose.connect('mongodb://localhost:27017/GraphQL', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log( 'Connected to MongoDB'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}/graphql`);
